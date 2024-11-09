aa97d7a39873c66b90095af60690d0bb3e88d883
Nigel Wright <nigel.wright2004@icloud.coml>
2024-11-09 14:38:31 -0500

    work on q3

---
+++ SQL/ex3.sql
@@ -0,0 +1,12 @@
+-- Insert statements used in Question 3 -- 
+-- 1st insert --
+INSERT INTO Department (departmentID, departmentName, address, postalCode) VALUES (1, 'Human Resources', '123 Maple St', '12345')
+-- 2nd insert --
+INSERT INTO Department
+SET departmentID = 2,
+    departmentName = 'Finance',
+    address = '456 Oak St',
+    postalCode = '23456';
+-- 3rd insert --
+INSERT INTO Department VALUES (3, 'Department_2', '982 Main St, City 2', '53525')
+
---
+++ SQL/ex5.sql
@@ -0,0 +1,14 @@
+-- Select statements used in Question 5 --
+-- 1st select -- 
+
+-- 2nd select -- 
+
+-- 3rd select -- 
+
+-- 4th select --  
+
+-- 5th select -- 
+
+-- 6th select -- 
+
+-- 7th select -- 
